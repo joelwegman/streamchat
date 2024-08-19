a82dacdf0dc7bcaea019e859d2aab2d611434be2
Joel Wegman <joel.wegman@gmail.com>
2024-08-19 13:09:42 -0700

    starter project from start.spring.io

---
+++ src/main/java/com/example/streamchat/StreamChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.streamchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamChatApplication.class, args);
+	}
+
+}
